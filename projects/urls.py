7ff9ed5b4511c4fe87c4547aecc601bf29f7c350
scristiansc <scristian1012@gmail.com>
2025-11-12 18:30:10 -0500

    Finally deployed and tested using Postman

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from projects.api import ProjectViewSet
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 
 router.register('api/projects', ProjectViewSet, 'projects')
 
